1766ff943a9c2ec10643b59f9c3468b0ba0fe846
Yusuf Eryilmaz <yusuferyilmaz1819@gmail.com>
2021-02-18 13:56:41 -0500

    wp message

--- src/Chat.js
+++ src/Chat.js
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Chat.css";
 import { Avatar, IconButton } from "@material-ui/core";
-import { SearchOutlined, AttachFile, MoreVert } from "@material-ui/icons";
+import {
+  SearchOutlined,
+  AttachFile,
+  MoreVert,
+  InsertEmoticon,
+  Mic,
+} from "@material-ui/icons";
 
 function Chat() {
+  const [input, setInput] = useState();
+  function sendMessage() {
+    console.log(input);
+  }
+
   return (
     <div className="chat">
       <div className="chat__header">
@@ -25,12 +36,32 @@ function Chat() {
         </div>
       </div>
       <div className="chat__body">
-        <p>
+        <p className="chat__message">
+          <span className="chat__name">Yusuf</span>
           This is a message
-          <span className="chat__timestamp">
-            {new Date().toLocaleDateString()}
-          </span>
+          <span className="chat__timestamp">{new Date().toLocaleString()}</span>
         </p>
+        <p className="chat__message chat__receiver">
+          <span className="chat__name">Yasin</span>
+          This is a message
+          <span className="chat__timestamp">{new Date().toLocaleString()}</span>
+        </p>
+      </div>
+      <div className="chat__footer">
+        <InsertEmoticon />
+        <form>
+          <input
+            onChange={(e) => {
+              setInput(e.target.value);
+            }}
+            placeholder="Type a message"
+            type="text"
+          />
+          <button onClick={sendMessage} type="submit">
+            Send Message
+          </button>
+        </form>
+        <Mic />
       </div>
     </div>
   );
