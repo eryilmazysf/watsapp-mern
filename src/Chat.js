9f9f08a27de6da5c37aba7e0b4f78fbd5e2da031
Yusuf Eryilmaz <yusuferyilmaz1819@gmail.com>
2021-02-20 13:15:26 -0500

    wp

--- src/Chat.js
+++ src/Chat.js
@@ -27,7 +27,7 @@ function Chat({ messages }) {
   return (
     <div className="chat">
       <div className="chat__header">
-        <Avatar />
+        <Avatar src="https://images.unsplash.com/photo-1494029722188-672a328c4989?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTB8fGNhcnRvb258ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" />
         <div className="chat__headerInfo">
           <h3>Room Name</h3>
           <p>Last Seen at...</p>
--- src/SidebarChat.js
+++ src/SidebarChat.js
@@ -5,7 +5,7 @@ import { Avatar } from "@material-ui/core";
 function SidebarChat() {
   return (
     <div className="sidebarChat">
-      <Avatar />
+      <Avatar src="https://images.unsplash.com/photo-1494029722188-672a328c4989?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTB8fGNhcnRvb258ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" />
       <div className="sidebarChat__info">
         <h2>Room Name</h2>
         <p>Message</p>
