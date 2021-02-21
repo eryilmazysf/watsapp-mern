da9a38ba0f9a0e5502a33c1ac3a927afb2b2c72b
Yusuf Eryilmaz <yusuferyilmaz1819@gmail.com>
2021-02-21 14:23:19 -0500

    url

--- src/axios.js
+++ src/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 const instance = axios.create({
-  baseURL: "http://wp-env.eba-bwdchcds.us-east-1.elasticbeanstalk.com/",
+  baseURL: "http://wp-env.eba-bwdchcds.us-east-1.elasticbeanstalk.com",
 });
 export default instance;
